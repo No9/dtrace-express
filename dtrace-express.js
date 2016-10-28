0361a86e42d5e694a0a0750e31733c0081fe34c5
Anton Whalley <anton@venshare.com>
2016-10-28 09:59:57 +0100

    Provide chrome trace compatible output

--- dtrace-express.js
+++ dtrace-express.js
@@ -1,22 +1,24 @@
 var d = require('dtrace-provider');
 var provider = d.createDTraceProvider('dtrace-express');
-var probe = provider.addProbe('trace', 'char *', 'char *');
-
+var probe = provider.addProbe('trace', 'char *', 'char *', 'int', 'char *');
+var resid = 0;
 provider.enable();
 
 module.exports.instrument = function(options) {
    probe.fire(function(p) {
-      return [options.event, options.args.join(' ')];
+      return [options.event, options.args[0], options.args[1]];
    });
 }
 
 module.exports.start = function(req, res, next) {
-   res.start = process.hrtime();
+   var t = process.hrtime();
+   resid++;
+   res.id = resid;
+   res.trace(resid + ":" + req.path, "B", (t[0] * 1e9 + t[1]) / 1000 );
    next();
 }
 
 module.exports.finish = function(req, res, next) {
-   var t = process.hrtime(res.start);
-   var total = (t[0] * 1e9 + t[1]) / 1000000 
-   res.trace('finish', req.connection.remoteAddress, req.path, total);
+  var t = process.hrtime();
+  res.trace(res.id+ ":" + req.path, "E",  (t[0] * 1e9 + t[1]) / 1000);
 }
---
+++ examples/chrome-out.d
@@ -0,0 +1,22 @@
+#pragma D option strsize=4k 
+#pragma D option quiet
+/*{"name": "Asub", "cat": "PERF", "ph": "B", "pid": 22630, "tid": 22630, "ts": 829} , copyinstr(arg0), copyinstr(arg1)*/
+
+BEGIN {
+	printf("[");
+}
+:::trace
+/copyinstr(arg1) == "B" || copyinstr(arg1) == "E"/{
+	printf("{ \"name\": \"%s\", \"cat\": \"PERF\", \"ph\": \"%s\", \"pid\":%d, \"tid\":%d, \"ts\":%d},\n"
+			, copyinstr(arg0), copyinstr(arg1),  pid, tid, arg2); 
+}
+
+:::trace 
+/copyinstr(arg1) != "B" && copyinstr(arg1) != "E"/{
+	printf("{ \"name\": \"%s\", \"cat\": \"PERF\", \"ph\": \"i\", \"pid\":%d, \"tid\":%d, \"ts\":%d},\n"
+			, copyinstr(arg0), pid, tid, vtimestamp / 1000); 
+}
+
+END {
+	printf("]");
+}
