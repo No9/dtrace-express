f25fa80b92d2ad789c6a4a69ef16dc9d3ee142a5
anton whalley <anton@venshare.com>
2016-11-22 19:18:01 +0000

    Simplified the example

--- examples/index.js
+++ examples/index.js
@@ -12,21 +12,9 @@ function random(){
 // Add a middleware that runs trace function.
 app.use(dte.start);
 
-// Add another middleware that runs trace after an asynchronous call.
-app.use(function(req, res, next){
-  res.trace('wait:before');
-  setTimeout(function () {
-    res.trace('wait:after');
-    next();
-  }, Math.random() * 100);
-});
-
 // Add a route to show request traces.
 app.get('/', function(req, res, next){
-  res.trace('user.id', random());
-  res.trace('some.event', random(), random());
-  res.trace('another.event', random(), random());
-  res.set('X-Response-Time', (Date.now() - res.start) + 'ms');
+  res.trace('some.event', 'some event data');
   res.send('Hello world!');
   next();
 });
